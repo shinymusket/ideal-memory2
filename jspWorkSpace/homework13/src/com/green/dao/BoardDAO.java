9062a4c848b34b629308685d9c836119a078e763
shinymusket <grace211340@gmail.com>
2022-07-07 17:02:09 +0900

    homework13 myBatis

--- jspWorkSpace/homework13/src/com/green/dao/BoardDAO.java
+++ jspWorkSpace/homework13/src/com/green/dao/BoardDAO.java
@@ -1,5 +1,7 @@
 package com.green.dao;
 
+import java.io.IOException;
+import java.io.Reader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,8 +9,12 @@ import java.sql.Statement;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.green.vo.BoardVO;
 
@@ -16,7 +22,9 @@ import DBPKG.DBManager;
 
 public class BoardDAO {
 	
-	private BoardDAO() {};
+	private SqlSession session;
+	
+	private BoardDAO() {}
 	
 	private static BoardDAO dao = new BoardDAO();
 	
@@ -24,160 +32,242 @@ public class BoardDAO {
 		return dao;
 	}
 	
+	// myBatis
+	private SqlSession getSession() {
+		Reader reader = null;
+		try {
+			reader = Resources.getResourceAsReader("./SqlMapConfig.xml");
+			SqlSessionFactory sf = new SqlSessionFactoryBuilder().build(reader);
+			session = sf.openSession();
+		
+		} catch(IOException e) {
+			System.out.println("getSession 에러 : " + e);
+			e.printStackTrace();
+		}
+
+		return session;
+	}
+	
+	
 	// 게시판 리스트 가져오기
 	public List<BoardVO> selectBoardList() {
 		List<BoardVO> list = new ArrayList<>();
-		String sql = "SELECT idx, writer, subject, regdate FROM board_tbl ORDER BY regdate DESC";
-		
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		
 		try {
-			conn = DBManager.getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery(sql);
+			session = getSession();
+			list = session.selectList("board.board_select_list");
 			
-			while(rs.next()) {
-				BoardVO bVo = new BoardVO();
-				bVo.setIdx(rs.getInt(1));
-				bVo.setWriter(rs.getString(2));
-				bVo.setSubject(rs.getString(3));
-				
-				Timestamp timeStamp = rs.getTimestamp(4);
-				SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd kk:mm");
-				String newTimestamp = sdf.format(timeStamp); 
-				
-				bVo.setRegdate(newTimestamp);
-				
-				list.add(bVo);
-			}
-			
-			
-		} catch(Exception e) {
+		} catch (Exception e) {
+			System.out.println("selectBoardList 에러 : " + e);
 			e.printStackTrace();
-			
-		} finally {
-			DBManager.close(conn, stmt, rs);
 		}
-		return list;
 		
+		return list;
 	}
+	
+	
+//	public List<BoardVO> selectBoardList() {
+//		List<BoardVO> list = new ArrayList<>();
+//		String sql = "SELECT idx, writer, subject, regdate FROM board_tbl ORDER BY regdate DESC";
+//		
+//		Connection conn = null;
+//		Statement stmt = null;
+//		ResultSet rs = null;
+//		
+//		
+//		try {
+//			conn = DBManager.getConnection();
+//			stmt = conn.createStatement();
+//			rs = stmt.executeQuery(sql);
+//			
+//			while(rs.next()) {
+//				BoardVO bVo = new BoardVO();
+//				bVo.setIdx(rs.getInt(1));
+//				bVo.setWriter(rs.getString(2));
+//				bVo.setSubject(rs.getString(3));
+//				
+//				Timestamp timeStamp = rs.getTimestamp(4);
+//				SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd kk:mm");
+//				String newTimestamp = sdf.format(timeStamp); 
+//				
+//				bVo.setRegdate(newTimestamp);
+//				
+//				list.add(bVo);
+//			}
+//			
+//			
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//			
+//		} finally {
+//			DBManager.close(conn, stmt, rs);
+//		}
+//		return list;
+//		
+//	}
 
 	
 	// 게시물 조회하기
-
 	public BoardVO selectBoardViewByIdx(int idx) {
 		BoardVO bVo = new BoardVO();
-		String sql = "SELECT * FROM board_tbl WHERE idx=?";
-		Connection conn = null;
-		PreparedStatement psmt = null;
-		ResultSet rs = null;
-		
 		try {
-			conn = DBManager.getConnection();
-			psmt = conn.prepareStatement(sql);
-			psmt.setInt(1, idx);
-			rs = psmt.executeQuery();
-			
-			if (rs.next()) {
-				bVo.setIdx(rs.getInt(1));
-				bVo.setWriter(rs.getString(2));
-				bVo.setSubject(rs.getString(3));
-				bVo.setContent(rs.getString(4));
-				
-				Timestamp regdate = rs.getTimestamp(5);
-				SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd kk:mm:ss");
-				String newRegdate = sdf.format(regdate); 
-				bVo.setRegdate(newRegdate);
-			}
-			
-			
+			session = getSession();
+			bVo = session.selectOne("board.select_board", idx);
 		} catch(Exception e) {
+			System.out.println("selectBoardViewByIdx 에러 : " + e);
 			e.printStackTrace();
-		} finally {
-			DBManager.close(conn, psmt, rs);
 		}
-		
 		return bVo;
 	}
+	
+
+//	public BoardVO selectBoardViewByIdx(int idx) {
+//		BoardVO bVo = new BoardVO();
+//		String sql = "SELECT * FROM board_tbl WHERE idx=?";
+//		Connection conn = null;
+//		PreparedStatement psmt = null;
+//		ResultSet rs = null;
+//		
+//		try {
+//			conn = DBManager.getConnection();
+//			psmt = conn.prepareStatement(sql);
+//			psmt.setInt(1, idx);
+//			rs = psmt.executeQuery();
+//			
+//			if (rs.next()) {
+//				bVo.setIdx(rs.getInt(1));
+//				bVo.setWriter(rs.getString(2));
+//				bVo.setSubject(rs.getString(3));
+//				bVo.setContent(rs.getString(4));
+//				
+//				Timestamp regdate = rs.getTimestamp(5);
+//				SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd kk:mm:ss");
+//				String newRegdate = sdf.format(regdate); 
+//				bVo.setRegdate(newRegdate);
+//			}
+//			
+//			
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			DBManager.close(conn, psmt, rs);
+//		}
+//		
+//		return bVo;
+//	}
 
 	// 게시물 추가하기
 	public void insertBoard(BoardVO bVo) {
-		String sql = "INSERT INTO board_tbl VALUES(SEQ_BOARD_TBL.nextval, ?, ?, ?, sysdate)";
-		
-		Connection conn = null;
-		PreparedStatement psmt = null;
-		
 		try {
-			conn = DBManager.getConnection();
-			psmt = conn.prepareStatement(sql);
-					
-			psmt.setString(1, bVo.getWriter());
-			psmt.setString(2, bVo.getSubject());
-			psmt.setString(3, bVo.getContent());
+			session = getSession();
+			session.insert("board.insert_board", bVo);
+			session.commit(); // 반드시 커밋을 해줘야함.
 			
-			psmt.executeUpdate();
-					
 		} catch(Exception e) {
+			System.out.println("insertBoard 에러 : " + e);
 			e.printStackTrace();
-		} finally {
-			DBManager.close(conn, psmt);
 		}
-		
 	}
-
 	
+
+//	public void insertBoard(BoardVO bVo) {
+//		String sql = "INSERT INTO board_tbl VALUES(SEQ_BOARD_TBL.nextval, ?, ?, ?, sysdate)";
+//		
+//		Connection conn = null;
+//		PreparedStatement psmt = null;
+//		
+//		try {
+//			conn = DBManager.getConnection();
+//			psmt = conn.prepareStatement(sql);
+//					
+//			psmt.setString(1, bVo.getWriter());
+//			psmt.setString(2, bVo.getSubject());
+//			psmt.setString(3, bVo.getContent());
+//			
+//			psmt.executeUpdate();
+//					
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			DBManager.close(conn, psmt);
+//		}
+//		
+//	}
+
 	// 게시물 수정하기
 	public void updateBoard(BoardVO bVo) {
-		String sql = "UPDATE board_tbl SET writer=?, subject=?, content=? WHERE idx=?";
-		
-		Connection conn = null;
-		PreparedStatement psmt = null;
-		
 		try {
-			conn = DBManager.getConnection();
-			psmt = conn.prepareStatement(sql);
-			
-			psmt.setString(1, bVo.getWriter());
-			psmt.setString(2, bVo.getSubject());
-			psmt.setString(3, bVo.getContent());
-			psmt.setInt(4, bVo.getIdx());
-			
-			psmt.executeUpdate();
+			session = getSession();
+			session.update("board.update_board", bVo);
+			session.commit();
 			
 		} catch(Exception e) {
 			e.printStackTrace();
-		} finally {
-			DBManager.close(conn, psmt);
+			System.out.println("updateBoard 에러 : " + e);
 		}
-		
-		
 	}
+	
+	
+//	public void updateBoard(BoardVO bVo) {
+//		String sql = "UPDATE board_tbl SET writer=?, subject=?, content=? WHERE idx=?";
+//		
+//		Connection conn = null;
+//		PreparedStatement psmt = null;
+//		
+//		try {
+//			conn = DBManager.getConnection();
+//			psmt = conn.prepareStatement(sql);
+//			
+//			psmt.setString(1, bVo.getWriter());
+//			psmt.setString(2, bVo.getSubject());
+//			psmt.setString(3, bVo.getContent());
+//			psmt.setInt(4, bVo.getIdx());
+//			
+//			psmt.executeUpdate();
+//			
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			DBManager.close(conn, psmt);
+//		}
+//		
+//		
+//	}
 
 	// 게시물 삭제하기
 	public void deleteBoardByIdx(int idx) {
-		String sql = "DELETE board_tbl WHERE idx=?";
-		
-		Connection conn = null;
-		PreparedStatement psmt = null;
-		
 		try {
-			conn = DBManager.getConnection();
-			psmt = conn.prepareStatement(sql);
-			
-			psmt.setInt(1, idx);
-			
-			
-			psmt.executeUpdate();
+			session = getSession();
+			session.delete("board.delete_board", idx);
+			session.commit();
 			
 		} catch(Exception e) {
 			e.printStackTrace();
-		} finally {
-			DBManager.close(conn, psmt);
+			System.out.println("deleteBoardByIdx 에러 : " + e);
 		}
-		
-		
 	}
+	
+	
+//	public void deleteBoardByIdx(int idx) {
+//		String sql = "DELETE board_tbl WHERE idx=?";
+//		
+//		Connection conn = null;
+//		PreparedStatement psmt = null;
+//		
+//		try {
+//			conn = DBManager.getConnection();
+//			psmt = conn.prepareStatement(sql);
+//			
+//			psmt.setInt(1, idx);
+//			
+//			
+//			psmt.executeUpdate();
+//			
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			DBManager.close(conn, psmt);
+//		}
+//		
+//		
+//	}
 }
--- jspWorkSpace/homework13/src/com/green/dao/CommentDAO.java
+++ jspWorkSpace/homework13/src/com/green/dao/CommentDAO.java
@@ -1,5 +1,7 @@
 package com.green.dao;
 
+import java.io.IOException;
+import java.io.Reader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,11 +10,18 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import com.green.vo.CommentVO;
 
 import DBPKG.DBManager;
 
 public class CommentDAO {
+		
+		private SqlSession session; 
 	
 		private CommentDAO() {};
 		
@@ -22,96 +31,155 @@ public class CommentDAO {
 			return dao;
 		}
 		
+		// myBatis
+		private SqlSession getSession() {
+			Reader reader = null;
+			try {
+				reader = Resources.getResourceAsReader("./SqlMapConfig.xml");
+				SqlSessionFactory sf = new SqlSessionFactoryBuilder().build(reader);
+				session = sf.openSession();
+				
+			} catch(IOException e) {
+				System.out.println("getSession 에러 : " + e);
+				e.printStackTrace();
+			}
+			return session;
+		}
+		
 		// 댓글 조회
 		public List<CommentVO> selectAllCommnet(int bidx) {
 			List<CommentVO> list = new ArrayList<>();
-			String sql = "SELECT * FROM comment_tbl WHERE bidx=?";
-			
-			Connection conn = null;
-			PreparedStatement psmt = null;
-			ResultSet rs = null;
 			
 			try {
-				conn = DBManager.getConnection();
-				psmt = conn.prepareStatement(sql);
-				
-				psmt.setInt(1, bidx);
-				rs = psmt.executeQuery();
-				
-				while(rs.next()) {
-					CommentVO cVo = new CommentVO();
-					cVo.setIdx(rs.getInt(1));
-					cVo.setBidx(rs.getInt(2));
-					cVo.setWriter(rs.getString(3));
-					cVo.setContent(rs.getString(4));
-					
-					Timestamp regdate = rs.getTimestamp(5);
-					SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd kk:mm:ss");
-					String newRegdate = sdf.format(regdate); 
-					
-					cVo.setRegdate(newRegdate);
-					list.add(cVo);
-				}
-				
-				
+				session = getSession();
+				list = session.selectList("comment.select_comment", bidx);
 				
 			} catch(Exception e) {
+				System.out.println("selectAllCommnet 에러 : " + e);
 				e.printStackTrace();
-			} finally {
-				DBManager.close(conn, psmt, rs);
 			}
-
-			return list;
+ 			
 			
-		}
+			return list;
+		}	
+		
+		
+//		public List<CommentVO> selectAllCommnet(int bidx) {
+//			List<CommentVO> list = new ArrayList<>();
+//			String sql = "SELECT * FROM comment_tbl WHERE bidx=?";
+//			
+//			Connection conn = null;
+//			PreparedStatement psmt = null;
+//			ResultSet rs = null;
+//			
+//			try {
+//				conn = DBManager.getConnection();
+//				psmt = conn.prepareStatement(sql);
+//				
+//				psmt.setInt(1, bidx);
+//				rs = psmt.executeQuery();
+//				
+//				while(rs.next()) {
+//					CommentVO cVo = new CommentVO();
+//					cVo.setIdx(rs.getInt(1));
+//					cVo.setBidx(rs.getInt(2));
+//					cVo.setWriter(rs.getString(3));
+//					cVo.setContent(rs.getString(4));
+//					
+//					Timestamp regdate = rs.getTimestamp(5);
+//					SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd kk:mm:ss");
+//					String newRegdate = sdf.format(regdate); 
+//					
+//					cVo.setRegdate(newRegdate);
+//					list.add(cVo);
+//				}
+//				
+//				
+//				
+//			} catch(Exception e) {
+//				e.printStackTrace();
+//			} finally {
+//				DBManager.close(conn, psmt, rs);
+//			}
+//
+//			return list;
+//			
+//		}
 
 		
 		// 댓글 삭제
 		public void deleteCommentByIdx(int idx) {
-			String sql = "DELETE FROM comment_tbl WHERE idx=?";
-			
-			Connection conn = null;
-			PreparedStatement psmt = null;
-			
 			try {
-				conn = DBManager.getConnection();
-				psmt = conn.prepareStatement(sql);
-				
-				psmt.setInt(1, idx);
-				
-				psmt.executeUpdate();
-				
+				session = getSession();
+				session.delete("comment.delete_comment", idx);
+				session.commit();
+
 			} catch(Exception e) {
 				e.printStackTrace();
-			} finally {
-				DBManager.close(conn, psmt);
+				System.out.println("deleteCommentByIdx 에러 : " + e);
 			}
-			
-			
 		}
+		
+//		public void deleteCommentByIdx(int idx) {
+//			String sql = "DELETE FROM comment_tbl WHERE idx=?";
+//			
+//			Connection conn = null;
+//			PreparedStatement psmt = null;
+//			
+//			try {
+//				conn = DBManager.getConnection();
+//				psmt = conn.prepareStatement(sql);
+//				
+//				psmt.setInt(1, idx);
+//				
+//				psmt.executeUpdate();
+//				
+//			} catch(Exception e) {
+//				e.printStackTrace();
+//			} finally {
+//				DBManager.close(conn, psmt);
+//			}
+//			
+//			
+//		}
 		
 		// 댓글 작성
 		public void insertComment(CommentVO cVo) {
-			String sql = "INSERT INTO comment_tbl VALUES(seq_comment_tbl.nextval, ?, ?, ?, sysdate)";
-			
-			Connection conn = null;
-			PreparedStatement psmt = null;
-			
 			try {
-				conn = DBManager.getConnection();
-				psmt = conn.prepareStatement(sql);
-				psmt.setInt(1, cVo.getBidx());
-				psmt.setString(2, cVo.getWriter());
-				psmt.setString(3, cVo.getContent());
-				
-				psmt.executeUpdate();
-				
+				session = getSession();
+				session.insert("comment.insert_comment", cVo);
+				session.commit();
+
 			} catch(Exception e) {
 				e.printStackTrace();
-			} finally {
-				DBManager.close(conn, psmt);
+				System.out.println("insertComment 에러 : " + e);
 			}
-			
 		}
+		
+		
+		
+		
+//		public void insertComment(CommentVO cVo) {
+//			String sql = "INSERT INTO comment_tbl VALUES(seq_comment_tbl.nextval, ?, ?, ?, sysdate)";
+//			
+//			Connection conn = null;
+//			PreparedStatement psmt = null;
+//			
+//			try {
+//				conn = DBManager.getConnection();
+//				psmt = conn.prepareStatement(sql);
+//				psmt.setInt(1, cVo.getBidx());
+//				psmt.setString(2, cVo.getWriter());
+//				psmt.setString(3, cVo.getContent());
+//				
+//				psmt.executeUpdate();
+//				
+//			} catch(Exception e) {
+//				e.printStackTrace();
+//			} finally {
+//				DBManager.close(conn, psmt);
+//			}
+//			
+//		}
 		
 	}
